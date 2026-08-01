556a66315f1b161c4daabcb057c213d2eb293c10
genniaguilar-bit <genni.aguilar@gmail.com>
2026-08-02 00:35:23 +0200

    Add files via upload

---
+++ mis-apps/app.py
@@ -0,0 +1,26 @@
+﻿import streamlit as st
+
+st.title("Indicador de Preocupación")
+
+verbal = st.checkbox("1. Expresión Verbal - El usuario expresa miedo")
+conductual = st.checkbox("2. Comportamiento - Signos de inquietud")
+
+total = 0
+if verbal:
+    total = total + 1
+if conductual:
+    total = total + 1
+
+st.divider()
+
+if total == 0:
+    st.success("🟢 NIVEL BAJO")
+    st.info("Conducta: Continuar con la atención habitual.")
+
+if total == 1:
+    st.warning("🟡 NIVEL MODERADO")
+    st.info("Conducta: Validar emoción y dar más información.")
+
+if total == 2:
+    st.error("🔴 NIVEL ALTO")
+    st.info("Conducta: Detener, ofrecer agua y avisar a supervisor.")
